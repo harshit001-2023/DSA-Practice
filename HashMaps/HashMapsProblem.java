219c9e98032ada1a4de547e881058004ac2ee366
harshit001-2023 <harshitpande938@gmail.com>
2025-09-05 22:45:23 +0530

    HashMap practice started [05/09/2025]

---
+++ HashMaps/HashMapsProblem.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class HashMapsProblem{
+
+public static void main(String[] args) {
+    HashMap<Integer, String> map = new HashMap<>();
+    map.put(1,"Harshit");
+    map.put(2,"Ram");
+    map.put(3,"Sham");
+
+    System.out.println(map);
+
+}
+}
